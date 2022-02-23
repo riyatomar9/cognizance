a181f4945b2c78c4dda2ef47150ca8b4b8d498ab
riyatomar9 <99641903+riyatomar9@users.noreply.github.com>
2022-02-23 23:01:45 +0530

    Add files via upload

---
+++ Task 4/Problem 2.py
@@ -0,0 +1,4 @@
+#Reading input string
+x=input("Input the String: ")
+#using slicing to get characters at even index
+print(x[0::2])
---
+++ Task 4/Problem1.py
@@ -0,0 +1,8 @@
+a=int(input("Enter the first number: "))#Getting the first number from the user
+b=int(input("Enter the second number: "))#Getting the second number from the user
+
+#Adding the two numbers
+sum=a+b
+
+#Printing the answer
+print ("sum of the two numbers:" , sum)
